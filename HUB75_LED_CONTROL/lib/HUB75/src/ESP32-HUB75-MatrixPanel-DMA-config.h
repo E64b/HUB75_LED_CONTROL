984d28b1bd269b6c876a215e7d5e1bb7a5ffbd6c
Mike <125614854+E64b@users.noreply.github.com>
2023-10-20 16:15:18 +0300

    Example

--- HUB75_LED_CONTROL/lib/HUB75/src/ESP32-HUB75-MatrixPanel-DMA-config.h
+++ HUB75_LED_CONTROL/lib/HUB75/src/ESP32-HUB75-MatrixPanel-DMA-config.h
@@ -5,7 +5,7 @@
 
 //сообщения отладки по UART
 #ifndef DISABLE_SERIAL_DEBUG
-  #define SERIAL_DEBUG
+#define SERIAL_DEBUG
 #endif  
 //прроцедуры инциализации регистров драйверов чрезе прямой доступ к выводам интерфейса 
 //оставлено на всякий случай
@@ -15,38 +15,50 @@
 #define USE_COLORx16 //16-битный цвет для ICN2053 
 
 #ifndef BRIGHTNESS
- #define BRIGHTNESS 255
+#define BRIGHTNESS 255
 #endif
-enum{BRIGHTNESS_DEFAULT = BRIGHTNESS};
+enum{
+	BRIGHTNESS_DEFAULT=BRIGHTNESS
+	};
 
 #ifndef MATRIX_WIDTH
- #define MATRIX_WIDTH                80   // Single panel of 64 pixel width
+#define MATRIX_WIDTH                80   // Single panel of 64 pixel width
 #endif
-enum{PANEL_WIDTH = MATRIX_WIDTH};
+enum{
+	PANEL_WIDTH=MATRIX_WIDTH
+	};
 
 #ifndef MATRIX_HEIGHT
- #define MATRIX_HEIGHT               40   // CHANGE THIS VALUE to 64 IF USING 64px HIGH panel(s) with E PIN
+#define MATRIX_HEIGHT               40   // CHANGE THIS VALUE to 64 IF USING 64px HIGH panel(s) with E PIN
 #endif
-enum{PANEL_HEIGHT = MATRIX_HEIGHT};
+enum{
+	PANEL_HEIGHT=MATRIX_HEIGHT
+	};
 
 #ifndef PANEL_COUNT_WIDTH
- #define PANEL_COUNT_WIDTH                3   // Number of modules chained together, i.e. 4 panels chained result in virtualmatrix 64x4=256 px long
+#define PANEL_COUNT_WIDTH                3   // Number of modules chained together, i.e. 4 panels chained result in virtualmatrix 64x4=256 px long
 #endif
-enum{PANEL_COUNT_X = PANEL_COUNT_WIDTH};
+enum{
+	PANEL_COUNT_X=PANEL_COUNT_WIDTH
+	};
 
 #ifndef PANEL_COUNT_HEIGHT
- #define PANEL_COUNT_HEIGHT            3  // Number of modules chained together, i.e. 4 panels chained result in virtualmatrix 64x4=256 px long
+#define PANEL_COUNT_HEIGHT            3  // Number of modules chained together, i.e. 4 panels chained result in virtualmatrix 64x4=256 px long
 #endif
-enum{PANEL_COUNT_Y = PANEL_COUNT_HEIGHT};
+enum{
+	PANEL_COUNT_Y=PANEL_COUNT_HEIGHT
+	};
 
 
 #ifndef MATRIX_ROWS_IN_PARALLEL
- #define MATRIX_ROWS_IN_PARALLEL     2
+#define MATRIX_ROWS_IN_PARALLEL     2
 #endif
-enum{ROWS_IN_PARALLEL = MATRIX_ROWS_IN_PARALLEL};
+enum{
+	ROWS_IN_PARALLEL=MATRIX_ROWS_IN_PARALLEL
+	};
 
 #ifndef PIXEL_COLOR_DEPTH
- #define PIXEL_COLOR_DEPTH      COLORx16
+#define PIXEL_COLOR_DEPTH      COLORx16
 #endif
 
 //число строк DMA буфера для ICN2053 
@@ -55,7 +67,9 @@ enum{ROWS_IN_PARALLEL = MATRIX_ROWS_IN_PARALLEL};
 #ifndef DMA_ROW_BUFF_CNT
 #define DMA_ROW_BUFF_CNT 1
 #endif
-enum{ICN2053_DMA_ROW_BUFF_CNT = DMA_ROW_BUFF_CNT}; 
+enum{
+	ICN2053_DMA_ROW_BUFF_CNT=DMA_ROW_BUFF_CNT
+	};
 
 #ifndef SHIFT_DRIVER
 #define SHIFT_DRIVER ICN2053
@@ -146,45 +160,51 @@ enum{ICN2053_DMA_ROW_BUFF_CNT = DMA_ROW_BUFF_CNT};
 
 // Panel Upper half RGB (numbering according to order in DMA gpio_bus configuration)
 enum{
-  BITS_RGB1_OFFSET = 0, // Start point of RGB_X1 bits
-  BIT_R1 = (1<<0),   
-  BIT_G1 = (1<<1),   
-  BIT_B1 = (1<<2),   
+	BITS_RGB1_OFFSET=0, // Start point of RGB_X1 bits
+	BIT_R1=(1<<0),
+	BIT_G1=(1<<1),
+	BIT_B1=(1<<2),
 
-// Panel Lower half RGB
-  BITS_RGB2_OFFSET = 3, // Start point of RGB_X2 bits
-  BIT_R2 = (1<<3),   
-  BIT_G2 = (1<<4),   
-  BIT_B2 = (1<<5),   
+	// Panel Lower half RGB
+	BITS_RGB2_OFFSET=3, // Start point of RGB_X2 bits
+	BIT_R2=(1<<3),
+	BIT_G2=(1<<4),
+	BIT_B2=(1<<5),
 
-// Panel Control Signals
-  BIT_LAT = (1<<6), 
-  BIT_OE = (1<<7),  
+	// Panel Control Signals
+	BIT_LAT=(1<<6),
+	BIT_OE=(1<<7),
 
-// Panel GPIO Pin Addresses (A, B, C, D etc..)
-  BITS_ADDR_OFFSET = 8,  // Start point of address bits
-  BIT_A = (1<<8),    
-  BIT_B = (1<<9),    
-  BIT_C = (1<<10),   
-  BIT_D = (1<<11),  
-  BIT_E = (1<<12),  
-  #ifdef ADD_HSYNC_OUT 
-  BIT_HSYNC = (1<<14),
-  #endif
-  #ifdef ADD_VSYNC_OUT 
-  BIT_VSYNC = (1<<15),
-  #endif
+	// Panel GPIO Pin Addresses (A, B, C, D etc..)
+	BITS_ADDR_OFFSET=8,  // Start point of address bits
+	BIT_A=(1<<8),
+	BIT_B=(1<<9),
+	BIT_C=(1<<10),
+	BIT_D=(1<<11),
+	BIT_E=(1<<12),
+	#ifdef ADD_HSYNC_OUT 
+	BIT_HSYNC=(1<<14),
+	#endif
+	#ifdef ADD_VSYNC_OUT 
+	BIT_VSYNC=(1<<15),
+	#endif
 
-  BITMASK_ADDR  = BIT_A + BIT_B + BIT_C + BIT_D + BIT_E,  //битовая маска адреса строки
-  BITMASK_RGB1  = BIT_R1 + BIT_G1 + BIT_B1,    //битовая маска цвета верхних строк
-  BITMASK_RGB2  = BIT_R2 + BIT_G2 + BIT_B2,    //битовая маска цвета нижних строк
-  BITMASK_RGB12 = BITMASK_RGB1 + BITMASK_RGB2, //битовая маска цвета
-};
+	BITMASK_ADDR=BIT_A+BIT_B+BIT_C+BIT_D+BIT_E,  //битовая маска адреса строки
+	BITMASK_RGB1=BIT_R1+BIT_G1+BIT_B1,    //битовая маска цвета верхних строк
+	BITMASK_RGB2=BIT_R2+BIT_G2+BIT_B2,    //битовая маска цвета нижних строк
+	BITMASK_RGB12=BITMASK_RGB1+BITMASK_RGB2, //битовая маска цвета
+	};
 
 //For control card, SDI is the C of    3-8 decoder, DCK is the A of    3-8 decoder, RCK    is the B of    3-8 decoder 
-enum{BIT_SDI =  BIT_C};
-enum{BIT_DTK = BIT_A};
-enum{BIT_RCK = BIT_B};
+enum{
+	BIT_SDI=BIT_C
+	};
+enum{
+	BIT_DTK=BIT_A
+	};
+enum{
+	BIT_RCK=BIT_B
+	};
 
 extern const hub75_cfg_t hub75_cfg_default;
 
