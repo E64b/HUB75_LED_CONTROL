110f4b01004f5f4f65edeab2c9a9d661f5bdc3ac
Mike <125614854+E64b@users.noreply.github.com>
2023-10-21 04:03:28 +0300

    Example

--- HUB75_LED_CONTROL/example/example.ino
+++ HUB75_LED_CONTROL/example/example.ino
@@ -1,147 +1,79 @@
-#include <ESP32-HUB75-MatrixPanel-I2S-DMA.h>
-
-/*--------------------- MATRIX GPIO CONFIG  -------------------------*/
-#define R1_PIN 25
-#define G1_PIN 26
-#define B1_PIN 27
-#define R2_PIN 14
-#define G2_PIN 12
-#define B2_PIN 13
-#define A_PIN 23
-#define B_PIN 19 // Changed from library default
-#define C_PIN 5
-#define D_PIN 17
-#define E_PIN -1
-#define LAT_PIN 4
-#define OE_PIN 15
-#define CLK_PIN 16
-
-
-/*--------------------- MATRIX PANEL CONFIG -------------------------*/
-#define PANEL_RES_X 16     // Number of pixels wide of each INDIVIDUAL panel module. 
-#define PANEL_RES_Y 16   // Number of pixels tall of each INDIVIDUAL panel module.
+#define PANEL_RES_X 16    // Number of pixels wide of each INDIVIDUAL panel module. 
+#define PANEL_RES_Y 16     // Number of pixels tall of each INDIVIDUAL panel module.
 #define PANEL_CHAIN 1      // Total number of panels chained one to another
 
-/*
-//Another way of creating config structure
-//Custom pin mapping for all pins
-HUB75_I2S_CFG::i2s_pins _pins={R1, G1, BL1, R2, G2, BL2, CH_A, CH_B, CH_C, CH_D, CH_E, LAT, OE, CLK};
-HUB75_I2S_CFG mxconfig(
-						64,   // width
-						64,   // height
-						 4,   // chain length
-					 _pins,   // pin mapping
-  HUB75_I2S_CFG::FM6126A      // driver chip
-);
+#include <ESP32-HUB75-MatrixPanel-I2S-DMA.h>
 
-*/
 MatrixPanel_I2S_DMA* dma_display=nullptr;
 
-// Module configuration
-HUB75_I2S_CFG mxconfig(
-	PANEL_RES_X,   // module width
-	PANEL_RES_Y,   // module height
-	PANEL_CHAIN    // Chain length
-);
-
-
-//mxconfig.gpio.e = -1; // Assign a pin if you have a 64x64 panel
-//mxconfig.clkphase = false; // Change this if you have issues with ghosting.
-//mxconfig.driver = HUB75_I2S_CFG::FM6126A; // Change this according to your pane.
-
-
-
-#include <FastLED.h>
-
-#include "Effects.h"
-Effects effects;
-
-#include "Drawable.h"
-#include "Playlist.h"
-//#include "Geometry.h"
-
-#include "Patterns.h"
-Patterns patterns;
-
-/* -------------------------- Some variables -------------------------- */
-unsigned long fps=0, fps_timer; // fps (this is NOT a matrix refresh rate!)
-unsigned int default_fps=30, pattern_fps=30;  // default fps limit (this is not a matrix refresh counter!)
-unsigned long ms_animation_max_duration=20000;  // 20 seconds
-unsigned long last_frame=0, ms_previous=0;
-
 void setup(){
-	/************** SERIAL **************/
-	Serial.begin(115200);
-	delay(250);
 
-	/************** DISPLAY **************/
-	Serial.println("...Starting Display");
-	dma_display=new MatrixPanel_I2S_DMA(mxconfig);
-	dma_display->begin();
-	dma_display->setBrightness8(90); //0-255
-
-	dma_display->fillScreenRGB888(128, 0, 0);
-	delay(1000);
-	dma_display->fillScreenRGB888(0, 0, 128);
-	delay(1000);
-	dma_display->clearScreen();
-	delay(1000);
-	Serial.println("**************** Starting Aurora Effects Demo ****************");
+    Serial.begin(115200);
 
 
-	// setup the effects generator
-	effects.Setup();
+    HUB75_I2S_CFG::i2s_pins _pins={
+        25, //R1_PIN, 
+        26, //G1_PIN, 
+        27, //B1_PIN, 
+        14, //R2_PIN, 
+        12, //G2_PIN, 
+        13, //B2_PIN, 
+        23, //A_PIN, 
+        19, //B_PIN, 
+        5, //C_PIN, 
+        17, //D_PIN, 
+        18, //E_PIN,
+        4, //LAT_PIN, 
+        15, //OE_PIN, 
+        16, //CLK_PIN
+        };
+    HUB75_I2S_CFG mxconfig(
+        PANEL_RES_X, // Module width
+        PANEL_RES_Y, // Module height
+        PANEL_CHAIN //, // chain length
+        //_pins // pin mapping -- uncomment if providing own custom pin mapping as per above.
+    );
+    //mxconfig.clkphase = false;
+    //mxconfig.driver = HUB75_I2S_CFG::FM6126A;
 
-	delay(500);
-	Serial.println("Effects being loaded: ");
-	listPatterns();
-
-
-	patterns.moveRandom(1); // start from a random pattern
-
-	Serial.print("Starting with pattern: ");
-	Serial.println(patterns.getCurrentPatternName());
-	patterns.start();
-	ms_previous=millis();
-	fps_timer=millis();
-	}
+    // Display Setup
+    dma_display=new MatrixPanel_I2S_DMA(mxconfig);
+    dma_display->begin();
+    dma_display->clearScreen();
+    }
 
 void loop(){
-	// menu.run(mainMenuItems, mainMenuItemCount);  
+    // Canvas loop
+    float t=(float)((millis()%4000)/4000.f);
+    float tt=(float)((millis()%16000)/16000.f);
 
-	if((millis()-ms_previous)>ms_animation_max_duration){
-		patterns.stop();
-		patterns.moveRandom(1);
-		//patterns.move(1);
-		patterns.start();
+    for(int x=0; x<(PANEL_RES_X*PANEL_CHAIN); x++){
+        // calculate the overal shade
+        float f=(((sin(tt-(float)x/PANEL_RES_Y/32.)*2.f*PI)+1)/2)*255;
+        // calculate hue spectrum into rgb
+        float r=max(min(cosf(2.f*PI*(t+((float)x/PANEL_RES_Y+0.f)/3.f))+0.5f, 1.f), 0.f);
+        float g=max(min(cosf(2.f*PI*(t+((float)x/PANEL_RES_Y+1.f)/3.f))+0.5f, 1.f), 0.f);
+        float b=max(min(cosf(2.f*PI*(t+((float)x/PANEL_RES_Y+2.f)/3.f))+0.5f, 1.f), 0.f);
 
-		Serial.print("Changing pattern to:  ");
-		Serial.println(patterns.getCurrentPatternName());
-
-		ms_previous=millis();
-
-		// Select a random palette as well
-		//effects.RandomPalette();
-		}
-
-	if(1000/pattern_fps+last_frame<millis()){
-		last_frame=millis();
-		pattern_fps=patterns.drawFrame();
-		if(!pattern_fps)
-			pattern_fps=default_fps;
-
-		++fps;
-		}
-
-	if(fps_timer+1000<millis()){
-		Serial.printf_P(PSTR("Effect fps: %ld\n"), fps);
-		fps_timer=millis();
-		fps=0;
-		}
-
-	}
-
-
-void listPatterns(){
-	patterns.listPatterns();
-	}
+        // iterate pixels for every row
+        for(int y=0; y<PANEL_RES_Y; y++){
+            if(y*2<PANEL_RES_Y){
+                // top-middle part of screen, transition of value
+                float t=(2.f*y+1)/PANEL_RES_Y;
+                dma_display->drawPixelRGB888(x, y,
+                                             (r*t)*f,
+                                             (g*t)*f,
+                                             (b*t)*f
+                );
+                } else{
+                // middle to bottom of screen, transition of saturation
+                float t=(2.f*(PANEL_RES_Y-y)-1)/PANEL_RES_Y;
+                dma_display->drawPixelRGB888(x, y,
+                                             (r*t+1-t)*f,
+                                             (g*t+1-t)*f,
+                                             (b*t+1-t)*f
+                );
+                }
+            }
+        }
+    }
